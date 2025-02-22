4f31a1b16b9161d4bec9e734f3bbca2accc6d00d
NguyenDonUET <nguyendoncb@gmail.com>
2025-02-22 19:53:03 +0700

    feat: play video modal

--- js/main.js
+++ js/main.js
@@ -5,6 +5,35 @@ mobileNavToggle.addEventListener("click", () => {
   mobileNav.classList.toggle("mobile-nav--open")
 })
 
+// Video Modal
+const modal = document.getElementById("videoModal")
+const videoButton = document.querySelector(".preview__video-button")
+const closeButton = document.querySelector(".modal__close-button")
+const videoPlayer = document.getElementById("videoPlayer")
+
+// Open modal when clicked
+videoButton.addEventListener("click", function () {
+  // Show modal
+  modal.style.display = "block"
+
+  //Replace the src attribute with the video URL
+  videoPlayer.src = "https://www.youtube.com/embed/8sXRyHI3bLw"
+
+  // Close modal on close button click
+  closeButton.addEventListener("click", function () {
+    modal.style.display = "none"
+    videoPlayer.src = ""
+  })
+
+  // Close modal on outter click
+  window.addEventListener("click", function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none"
+      videoPlayer.src = ""
+    }
+  })
+})
+
 // Navigation background on scroll
 window.addEventListener("scroll", function () {
   const mainHeader = document.querySelector(".main-header")
