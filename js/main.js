0fd2b8d91059de0c96cc9ebc768c8563cd06f1f9
NguyenDonUET <nguyendoncb@gmail.com>
2025-02-22 18:31:23 +0700

    feat: add navbar background on scroll

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ const mobileNavToggle = document.querySelector(".mobile-menu__btn")
 mobileNavToggle.addEventListener("click", () => {
   mobileNav.classList.toggle("mobile-nav--open")
 })
+
+// Navigation background on scroll
+window.addEventListener("scroll", function () {
+  const mainHeader = document.querySelector(".main-header")
+  if (window.scrollY > 0) {
+    mainHeader.classList.add("main-header--scrolled")
+  } else {
+    mainHeader.classList.remove("main-header--scrolled")
+  }
+})
